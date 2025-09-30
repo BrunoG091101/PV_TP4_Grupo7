f8f5f37ca2b55b233e0e5766166827e9867851a9
eze <ezep25728@gmail.com>
2025-09-29 23:41:54 -0300

    actualizacion de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
 import './App.css'
+import Numero from './assets/componentes/numero'
+import Validar from './assets/componentes/Validar'
 import Saludo from './assets/componentes/saludo'
 import Botones from './assets/componentes/Botones'
 
 function App() {
-  
+  let numeroMax=100;
+  let numeroMin=1;
+  let random=Numero(numeroMin,numeroMax);
+
   return (
     <>
       <Saludo/>
-
+      <Validar random={random}/>
       <Botones/>
     </>
   )
